08e94c055cbdb595744be9dd682faa7a2ed4efa6
bruno <bruno-schmidt@users.noreply.github.com>
2019-07-16 06:19:13 -0300

    Remove a test that looks unnecessary
    In this test I was testing if the repository is being called
    (it shouldnt) when the date validation fails.
    I don't think it is useful, so I'm removing it.
    In the future I will test if I'm getting the correct error message
    when something bad happens.

--- test/unit/makeAddEvent.test.js
+++ test/unit/makeAddEvent.test.js
@@ -22,20 +22,5 @@ describe('makeAddEvent()', () => {
             expect(error).toBe(false)
             expect(value.id).toBe(123)
         })
-
-        it('should not call createEvent if the date is invalid', () => {
-            const createEventMock = jest.fn(({ title, date, description }) => {
-                return { id: 456, title, date, description }
-            })
-
-            const validateEventDateMock = jest.fn((date) => {
-                return { error: true, value: date }
-            })
-
-            const addEvent = makeAddEvent(createEventMock, validateEventDateMock)
-
-            addEvent({ title: 'Teste', date: '2010-01-01', description: 'Testando'})
-            expect(createEventMock).not.toBeCalled()
-        })
     })    
 })
